9a321279d4a5ca371a656bbbfc18f1673e5f5213
primaam <pmaharyono@gmail.com>
2024-08-27 23:58:55 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
     return (
-        <>
+        <Provider store={store}>
             <AppRoutes />
-        </>
+        </Provider>
     );
 }
 
--- src/redux/reducer/userRed.ts
+++ src/redux/reducer/userRed.ts
@@ -17,9 +17,40 @@ const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers:{
-        fetchUserStart(state){
+        getUsersStart(state){
             state.loading = true;
             state.error = null
+        },
+        getUsersSuccess(state, action: PayloadAction<User[]>){
+            state.users = action.payload
+            state.loading = false;
+        },
+        getUsersFailed(state, action: PayloadAction<string>){
+            state.loading = false
+            state.error = action.payload
+        },
+        addUser(state, action: PayloadAction<User>){
+            state.users.push(action.payload)
+        },
+        editUser(state, action: PayloadAction<User>){
+            const id = state.users.findIndex((item)=> {return item.id === action.payload.id})
+            if(id !== undefined){
+                state.users[id] = action.payload
+            }
+        },
+        deleteUser(state, action: PayloadAction<User>){
+            state.users = state.users.filter((item) =>{return item.id !== action.payload.id })
         }
     }
 })
+
+export const{
+    addUser,
+    deleteUser,
+    editUser,
+    getUsersFailed,
+    getUsersStart,
+    getUsersSuccess
+} = userSlice.actions
+
+export default userSlice.reducer
